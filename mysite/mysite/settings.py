0dc3ab5ed71e5d2ca5f67d0d5a0c9f6698e2805a
mhiga701 <112960364+mhiga701@users.noreply.github.com>
2023-04-07 09:22:38 -0400

    debug

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     "django.urls",
     "spotipy",
     "spotipy.oauth2",
-    "pymongo",
     
 ]
 
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, HttpResponseRedirect
 import spotipy
 from spotipy import oauth2
 import spotipy.util as util
+from django.shortcuts import render
+import requests
+
 scope="user-library-read"
 CLIENT_ID = "c94210b17c5d4338a4a85a89ad3eeda6"
 CLIENT_SECRET = "d5969d54ba64496d9e2c6b9c2f4580d1"
@@ -63,3 +66,38 @@ def after_sign_in(request):
 # for idx, item in enumerate(results['items']):
 #     track = item['track']
 #     print(idx, track['artists'][0]['name'], " – ", track['name'])
+
+#####################
+#Accuweather
+API = "xKGPVZlAPvwPem2aQFN2LEAns6Awplp5"
+
+
+key = ""
+
+def getWeather(city):
+    city = input("Enter A City Name: ").replace(" ", "")
+    search_address = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=xKGPVZlAPvwPem2aQFN2LEAns6Awplp5&q="+city+"&language=en-us&details=false&alias=27"
+    response = requests.get(search_address)
+    data = response.json()
+    #print(search_address)
+    #read city data
+    # with urllib.request.urlopen(search_address) as search_address:
+    #     data=json.loads(search_address.read().decode())
+
+    #find the 6 digit location key
+    locationKey = data[0]['Key']
+    dailyForecast = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/"+locationKey+"?apikey=xKGPVZlAPvwPem2aQFN2LEAns6Awplp5&language=en-us&details=false&metric=false"
+    response = requests.get(dailyForecast)
+    data1 = response.json()
+    lowtemp = str(data1['Temperature']['Minimum']['Value'])
+    hightemp = str(data1['Temperature']['Maximum']['Value'])
+    forecast = str(data1['Day']['IconPhrase'])
+
+    context = {
+        "low": lowtemp,
+        "high: ": hightemp,
+        "daily: ": forecast
+    }
+
+    return render(city, "accuweather.html", context)
+ 
