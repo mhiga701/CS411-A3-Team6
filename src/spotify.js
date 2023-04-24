57ab00b85ccfb450399497bf01b4fb369a971157
mhiga701 <112960364+mhiga701@users.noreply.github.com>
2023-04-23 22:10:05 -0400

    bugs fixed

--- src/spotify.js
+++ src/spotify.js
@@ -16,8 +16,8 @@ const LOCALSTORAGE_KEYS = {
   };
 
 export const logout = () => {
-    for (const param in LOCALSTORAGE_KEYS) {
-        window.localStorage.removeItem(LOCALSTORAGE_KEYS[param]);
+    for (const property in LOCALSTORAGE_KEYS) {
+        window.localStorage.removeItem(LOCALSTORAGE_KEYS[property]);
     }
     window.location = window.location.origin;
 };
@@ -54,38 +54,37 @@ const refreshToken = async () => {
 const getAccessToken = () => {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
+
     const queryParams = {
-      [LOCALSTORAGE_KEYS.accessToken]: urlParams.get('access_token'),
-      [LOCALSTORAGE_KEYS.refreshToken]: urlParams.get('refresh_token'),
-      [LOCALSTORAGE_KEYS.expireTime]: urlParams.get('expires_in'),
+        [LOCALSTORAGE_KEYS.accessToken]: urlParams.get('access_token'),
+        [LOCALSTORAGE_KEYS.refreshToken]: urlParams.get('refresh_token'),
+        [LOCALSTORAGE_KEYS.expireTime]: urlParams.get('expires_in'),
     };
+
     const hasError = urlParams.get('error');
-  
-    // If there's an error OR the token in localStorage has expired, refresh the token
+
     if (hasError || tokenExpired() || LOCALSTORAGE_VALUES.accessToken === 'undefined') {
-      refreshToken();
+        refreshToken();
     }
-  
-    // If there is a valid access token in localStorage, use that
+
     if (LOCALSTORAGE_VALUES.accessToken && LOCALSTORAGE_VALUES.accessToken !== 'undefined') {
-      return LOCALSTORAGE_VALUES.accessToken;
+        return LOCALSTORAGE_VALUES.accessToken;
     }
-  
-    // If there is a token in the URL query params, user is logging in for the first time
+
+    //first time login by user, no store access token yet but it exists in url
     if (queryParams[LOCALSTORAGE_KEYS.accessToken]) {
-      // Store the query params in localStorage
-      for (const property in queryParams) {
-        window.localStorage.setItem(property, queryParams[property]);
-      }
-      // Set timestamp
-      window.localStorage.setItem(LOCALSTORAGE_KEYS.timestamp, Date.now());
-      // Return access token from query params
-      return queryParams[LOCALSTORAGE_KEYS.accessToken];
+        for (const property in queryParams) {
+            window.localStorage.setItem(property, queryParams[property]);
+        }
+    //mark the time when access token is intially added to local storage
+    window.localStorage.setItem(LOCALSTORAGE_KEYS.timestamp, Date.now());
+
+    return queryParams[LOCALSTORAGE_KEYS.accessToken];
     }
-  
-    // We should never get here!
+//else case that shouldn't happen
     return false;
-  };
+
+};
 
 export const accessToken = getAccessToken();
 
