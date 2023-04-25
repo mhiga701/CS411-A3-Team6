db947152135052386a85acb8f375a0b06193bff7
mhiga701 <112960364+mhiga701@users.noreply.github.com>
2023-04-24 23:43:17 -0400

    stopping for the night

---
+++ src/pages/gmap.js
@@ -0,0 +1,5 @@
+let field1 = document.getElementById('from');
+let field2 = document.getElementById('to');
+
+let auto1 = new google.maps.places.Autocomplete(field1);
+let auto2 = new google.maps.places.Autocomplete(field2);
