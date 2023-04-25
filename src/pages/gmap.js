53417d562f6213fb5a95f03890a7ef683291eed3
mhiga701 <112960364+mhiga701@users.noreply.github.com>
2023-04-25 14:50:01 -0400

    cant figure out atuocomp

--- src/pages/gmap.js
+++ src/pages/gmap.js
@@ -1,9 +1,13 @@
-function autoComp() {
-    let field1 = document.getElementById('from');
-    let field2 = document.getElementById('to');
+var field1 = document.getElementById('from');
+var auto1 = new google.maps.places.Autocomplete(field1);
 
-    let auto1 = new google.maps.places.Autocomplete(field1);
-    let auto2 = new google.maps.places.Autocomplete(field2);
+var field2 = document.getElementById('to');
+var auto2 = new google.maps.places.Autocomplete(field2);
+
+    
 
 
-}
+
+
+
+
