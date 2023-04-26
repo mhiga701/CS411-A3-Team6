49b1c44aceea28fe17ee5e1663631e1568ea46d6
mhiga701 <112960364+mhiga701@users.noreply.github.com>
2023-04-25 20:55:45 -0400

    this shit is frustrating

--- src/pages/gmap.js
+++ src/pages/gmap.js
@@ -1,56 +1,88 @@
-//center map in boston
-var myLatLng = {
-    lat: 42.3601,
-    lng: -71.0589
-}
-    
 var mapOptions = {
-    center: myLatLng,
-    zoom: 7,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    center: {lat: 42.3601, lng: -71.0589},
+    zoom: 10,
+
 }
 
-var map = new google.maps.Map(document.getElementById('gmap'), mapOptions);
-var directionsService = new google.maps.DirectionsService();
-var directionsDisplay = new google.maps.DirectionsRenderer();
+map = new google.maps.Map(document.getElementById('gmap'), mapOptions);
 
-directionsDisplay.setMap(map);
+const directionsService = new google.maps.DirectionsService();
+const directionsRenderer = new google.maps.DirectionsRenderer();
+
+directionsRenderer.setMap(map);
+
 
 function getDist() {
-    var request = {
-        origin: document.getElementById('from').value,
-        destination: document.getElementById('to').value,
-        travelType: google.maps.TravelMode.WALKING,
-        unitType: google.maps.UnitSystem.CUSTOMARY
-    }
-    DirectionsService.route(request, (result, status) => {
-        if (status == google.maps.DirectionsStatus.OK) {
-            const output = document.querySelector('#output');
-            output.innerHTML() = 
-            "<div class='alert-info'> From: " + 
-            document.getElementById('from').value +
-            ".<br />To :" +
-            document.getElementById('to').value +
-            ".<br /> Walking distance: " +
-            result.routes[0].legs[0].distance.text +
-            ".<br />Duration: " +
-            result.routes[0].legs[0].duration.text + ".</div>";
+var request = {
+    origin: document.getElementById('from').value,
+    destination: document.getElementById('to').value,
+    travelType: document.getElementById('mode').value,
+    unitType: google.maps.UnitSystem.CUSTOMARY
+}
+directionsService.route(request, (result, status) => {
+    if (status === google.maps.DirectionsStatus.OK) {
+        const output = document.querySelector('output');
+        output.innerHTML= 
+        "<div class='alert-info'> From: " + 
+        document.getElementById('from').value +
+        ".<br />To :" +
+        document.getElementById('to').value +
+        ".<br /> Distance: " +
+        result.routes[0].legs[0].distance.text +
+        ".<br />Duration: " +
+        result.routes[0].legs[0].duration.text + ".</div>";
 
-            directionsDisplay.setDirections(result);
-        }
-        else {
-            directionsDisplay.setDirections({ routes: []});
-            map.setCenter(myLatLng);
-            output.innerHTML() = "<div class='alert-danger'> Could not retrieve walking distance. </div>";
-        }
-    });
+        directionsRenderer.setDirections(result);
+    }
+    else {
+        directionsRenderer.setDirections({ routes: []});
+        map.setCenter(mapOptions.center);
+        output.innerHTML = "<div class='alert-danger'> Unable to retrieve distance of your trip. </div>";
+    }
+});
 }
 
-//autocomplete for origin field
 var field1 = document.getElementById('from');
 var auto1 = new google.maps.places.Autocomplete(field1);
 
 //autocomplete for destination field
 var field2 = document.getElementById('to');
 var auto2 = new google.maps.places.Autocomplete(field2);
+
+// function initMap() {
+    
+//     const directionsRenderer = new google.maps.DirectionsRenderer();
+//     const directionsService = new google.maps.DirectionsService();
+//     const map = new google.maps.Map(document.getElementById('gmap'), mapOptions);
+//     directionsRenderer.setMap(map);
+
+//     getDist(directionsRenderer, directionsService);
+//     document.getElementById('mode').addEventListener("change", () => {
+//         getDist(directionsRenderer, directionsService);
+//     });
+// }
+
+
+
+// function getDist(directionsRenderer, directionsService) {
+//     const selectedMode = document.getElementById('mode').value;
+
+//     directionsService.route ({
+//         origin: document.getElementById('from').value,
+//         destination: document.getElementById('to').value,
+
+//         travelType: google.maps.TravelMode[selectedMode],
+//     })
+//     .then((response) => {
+//         directionsRenderer.setDirections(response);
+//     })
+//     .catch((e) => window.alert("Direction request failed due to " + window.status));
+
+
+
+
+// function autoComp() {//autocomplete for origin field
+
+// }
+
 
