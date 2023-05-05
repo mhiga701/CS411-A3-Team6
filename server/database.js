df555ad6c2b98a454f5a22428f6cba7513a28d67
Jeff Tedi <jtedi@bu.edu>
2023-05-05 00:52:30 -0400

    i just need to fetch the spotify access token, everthing else looks good.. hopefully?

--- server/database.js
+++ server/database.js
@@ -1,40 +1,39 @@
-const { MongoClient } = require('mongodb')
+const express = require('express');
+const axios = require('axios');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
-let dbConnection
+const app = express();
+const port = 3000;
+const mongoUri = 'http://localhost:3000/';
 
-module.exports = {
-    connectToDB: (callback) => {
-        MongoClient.connect('mongodb://localhost:3000/users')
-        .then((client) => {
-            dbConnection = client.db()
-            return callback()
-        })
-        .catch(err => {
-            console.log(err)
-            return callback(err)
-        })
-    },
-    getDB: () => dbConnection
-}
+app.get('/playlist', async (req, res) => {
+  try {
+    // Retrieve the playlist from Spotify API
+    const playlistId = await axios.get(`https://api.spotify.com/v1/playlists/${playlistId}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.SPOTIFY_ACCESS_TOKEN}`, // replace this bitch
+        'Content-Type': 'application/json',
+      },
+    });
 
+    // Store the playlist in MongoDB
+    const client = new MongoClient(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    await client.connect();
+    const database = client.db('mydb');
+    const playlists = database.collection('playlists');
+    await playlists.insertOne({ playlist });
 
+    res.status(200).send('Playlist stored successfully');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Something went wrong');
+  }
+});
 
-
-/*
-// getting-started.js
-const mongoose = require('mongoose');
-
-main().catch(err => console.log(err));
-
-const accounts = new mongoose.Schema({
-    username: String,
-})
-
-async function main() {
-  await mongoose.connect('http://localhost:3000/');
-  
-
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-}
-*/
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
